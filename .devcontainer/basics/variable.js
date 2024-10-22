3caef05e92da4de1381ae8ac65c5fb1fe443541e
rubab783 <rubab0592@gmail.com>
2024-10-22 09:41:17 +0000

    code config done

---
+++ .devcontainer/basics/variable.js
@@ -0,0 +1,8 @@
+var a=10;
+const id=5;
+let email="rbyk@gmail.com";
+var password="123";
+/*
+ prefer not to use var keyword  for declaring variables because of block and functional scope.
+*/
+console.table[a,id,email,password];
